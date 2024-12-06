efd1e2b95a4a6547594207e3b54de933ef1bde78
VonRiddarn <vonriddarn@gmail.com>
2024-12-06 13:50:04 +0100

    docs: cleaned up main for better readability

--- src/main.ts
+++ src/main.ts
@@ -34,31 +34,19 @@ const dogs = getDogsDummyArray();
 console.log(`Vi har ${dogs.length} stycken hundar.`);
 console.log(`Hundarna har ${Utilities.getUniqueValues(dogs, "color").length} antal unika färger.`);
 console.log(`Den vanligaste färgen bland alla hundar är: ${Utilities.getMostCommonValue(dogs, "color")}.`);
-// vi vill ha en pretty print som skriver ut alla färger som hundarna har och hur många hundar det finns av varje färg
 console.log(Utilities.mapToString(Utilities.getObjectsPerValue(dogs, "color")));
 console.log(Utilities.objectToStringAll(dogs));
 
-const monsters: Monster[] = getMonsterDummyArray();
 
+/////
+// MONSTER RELATED
+/////
+const monsters: Monster[] = getMonsterDummyArray();
 console.log(`Vi har ${monsters.length} stycken monster.`);
 console.log(`Medelåldern på alla monster är ${Utilities.getAverage(monsters, "age")}`);
-console.log(
-	`Medelantalet tentakler för alla monster är ${Utilities.getAverage(monsters, "tentacles")}`
-);
-
-// loopa igenom alla monster och skriv ut dem med prettyPrintMonster
-// Changed prettyPrintMonster to monsterToString / and added monsterToStringAll
-// This solution adheres to separation of concerns
+console.log(`Medelantalet tentakler för alla monster är ${Utilities.getAverage(monsters, "tentacles")}`);
 console.log(Utilities.objectToStringAll(monsters));
-
 console.log(`Antalet monster som har vingar är ${Utilities.filterWithBoolean(monsters, "hasWings").length}`);
 
 const noWingedMonster = Utilities.filterWithBoolean(monsters, "hasWings", true);
 console.log(noWingedMonster);
-
-console.log("User age average:");
-console.log(Utilities.getAverage(arr, "age"));
-console.log("Monster age average:");
-console.log(Utilities.getAverage(monsters, "age"));
-console.log("Monster tentacle average:");
-console.log(Utilities.getAverage(monsters, "tentacles"));
