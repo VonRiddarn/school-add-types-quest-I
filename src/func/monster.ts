7901c06b9b21d4afbf3de8ad7ee2c1f0cbce7e34
VonRiddarn <vonriddarn@gmail.com>
2024-12-06 09:07:14 +0100

    feat: implemented prettyPrintMonster and prettyPrintMonsterAll

--- src/func/monster.ts
+++ src/func/monster.ts
@@ -1,7 +1,34 @@
-//utöka så alla egenskaper för monstret skrivs ut
+export type Monster = {
+	name: string;
+	age: number;
+	tentacles: number;
+	eyes: number;
+	hasWings: boolean;
+};
 
-export const prettyPrintMonster = (m) => {
-	console.log(`Monster: ${m.name}`);
+export const prettyPrintMonsterAll = (m: Monster[]) => {
+	let s: string = "";
+
+	m.forEach((monster) => {
+		s += `${prettyPrintMonster(monster)}\n`;
+	});
+
+	return s;
+}
+
+export const prettyPrintMonster = (m: Monster) => {
+	let s: string = "----------\n";
+
+	for (const key in m) 
+	{
+        if (Object.prototype.hasOwnProperty.call(m, key)) {
+            s += `${key}: ${m[key as keyof Monster]}\n`;
+        }
+	}
+
+	s+= "----------";
+
+	return s;
 };
 
 export const averageMonsterAge = (m) => {
